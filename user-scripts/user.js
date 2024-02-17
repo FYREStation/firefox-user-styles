1068c6eeaebfe515b1d2ee5e0938b61ff9a33728
FYREStation <fyre5480@gmail.com>
2024-02-17 11:19:06 -0600

    Update user.js

--- user-scripts/user.js
+++ user-scripts/user.js
@@ -1,6 +1,6 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.toolbars.bookmarks.visibility", "never");
-user_pref("extensions.activeThemeID", "cheers-bold-colorway@mozilla.org");
+user_pref("extensions.activeThemeID", "{77b906e7-933c-47a7-809b-8ccf783afa5b}");
 user_pref("extensions.ui.extension.hidden", true);
 user_pref("extensions.pocket.enabled", false);
 user_pref("reader.parse-on-load.enabled", false);
